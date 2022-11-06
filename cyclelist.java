c67e1272ad0592feaa8064e4796cb6008736b184
unknown <nikhilchakravarthy1303@gmail.com>
2022-11-06 12:38:27 +0530

    cycle in a linked list detection

---
+++ cyclelist.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null)
+        {
+            return false;
+        }
+         ListNode slowPointer = head;
+         ListNode fastPointer = head.next;
+
+        while(fastPointer != null && fastPointer.next != null){
+            if(fastPointer == slowPointer)
+                return true;
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+        }
+        return false;
+
+    }
+}
