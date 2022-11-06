9bee95451e699f389242c8c8d62fc0abb2aec42e
unknown <nikhilchakravarthy1303@gmail.com>
2022-11-06 05:59:48 +0530

    jump

---
+++ jumpgame.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class jumpgame {
+    public boolean canJump(int[] nums) {
+        if(nums.length==1)
+        {
+            return true;
+        }
+        int dist = nums[0];
+        for(int i=0;i<nums.length;i++)
+        {
+            dist = Math.max(dist,nums[i]);
+            if(dist == 0 && i != nums.length-1)
+            {
+                return false;
+            }
+            dist--;
+        }
+        return true;
+    }
+}
