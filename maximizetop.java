20bfc1de38a2f009e5bc30bfd41295f09a652cf7
unknown <nikhilchakravarthy1303@gmail.com>
2022-11-30 05:37:54 +0530

    maximize top

---
+++ maximizetop.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumTop(int[] nums, int k) {
+        if(nums.length==1 && k%2!=0 )return -1;
+        int max=0;
+
+        for(int i=0;i<Math.min(k-1,nums.length);i++){
+            max=Math.max(max,nums[i]);
+        }
+        if(k<nums.length){
+            max=Math.max(max,nums[k]);
+        }
+        return max;
+    }
+}
