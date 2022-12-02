37c493a4d101b90d802b672ff56da275e9a6bf42
unknown <nikhilchakravarthy1303@gmail.com>
2022-12-02 06:09:06 +0530

    rotate string

--- rotatestring.java
+++ rotatestring.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        int index = s.indexOf(goal.charAt(0));
-        String res = "";
-        res += s.substring(index);
-        res += s.substring(0,index);
-        return res.equals(goal);
+        if(s.length()!=goal.length())
+        {
+            return false;
+        }
+        return (s+s).indexOf(goal)!=-1;
     }
 }
